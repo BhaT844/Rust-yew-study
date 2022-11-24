6a45ae86f08215599e1d08a9f9d77a48e8d6dde5
BhaT844 <bluehart_777@naver.com>
2022-11-24 10:17:33 +0900

    gh-pages commit

--- src/pages/mod.rs
+++
@@ -1,5 +0,0 @@
-mod home;
-mod product_detail;
-
-pub use home::Home;
-pub use product_detail::ProductDetail;
--- src/route.rs
+++
@@ -1,9 +0,0 @@
-use yew_router::prelude::*;
-
-#[derive(Switch, Debug, Clone)]
-pub enum Route {
-    #[to = "/product/{id}"]
-    ProductDetail(i32),
-    #[to = "/"]
-    HomePage,
-}
